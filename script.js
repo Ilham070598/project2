da5865f58b6d1229ac4275acab97636d2e0e394f
ilham <exaple@gmail.com>
2022-10-11 17:26:49 +0600

    использовал циклы

--- script.js
+++ script.js
@@ -7,12 +7,27 @@ const personalMovieDB = {
     genres: [],
     privat: false
 };
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('Насколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('Насколько оцените его?', '');
 
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('Насколько оцените его?', '');  
+if (a != null & b != null & a != "" & b != ""  & a.length<50)
+{
 personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+console.log("Ok");
+} else {
+    i--;
+    console.log("error")
+}
+}
+if (personalMovieDB.count <= 10) {
+    console.log("Слишком мало");
+} else if (personalMovieDB.count > 10 & personalMovieDB.count < 30 ) {
+    console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30)  {
+    console.log("Вы киноман!!!")
+} else {console.log("Error")}
+
+
 
 console.log(personalMovieDB);
